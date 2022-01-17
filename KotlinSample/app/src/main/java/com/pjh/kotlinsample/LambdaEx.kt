1d920878b78250033807a9a26e00142305ab3d48
pjhyng0125 <pjhyng0125@naver.com>
2022-01-18 00:10:42 +0900

    220118/pjh/lambda, collection api

---
+++ KotlinSample/app/src/main/java/com/pjh/kotlinsample/LambdaEx.kt
@@ -0,0 +1,6 @@
+package com.pjh.kotlinsample
+
+// 람다 : 익명 함수. 표현식을 줄인 것
+// 중괄호 안에 파라미터와 실행 코드 블럭을 정의
+// 다양한 Collection API 제공 (filter, map, flatmap, find, group by)
+val sum = {x:Int, y:Int -> x+y}
--- KotlinSample/app/src/test/java/com/pjh/kotlinsample/KotlinTest.kt
+++ KotlinSample/app/src/test/java/com/pjh/kotlinsample/KotlinTest.kt
@@ -85,4 +85,33 @@ class KotlinTest {
         Assert.assertEquals(f1, f2)
         Assert.assertEquals(f1.hashCode(), f2.hashCode())
     }
+
+    @Test
+    fun testLambdaEx() {
+        println(sum(1, 2))
+        Assert.assertEquals(4, sum(1,3))
+        Assert.assertEquals(4, { x: Int, y: Int -> x * y }(2, 2))
+        val exp = { x: Int, y: Int ->
+            { z: Int -> (x + y) * z}
+        }
+        val exp2 = exp(3, 2) // {(3+2)*z}
+        val result = exp2(4) // {(3+2)*4}
+        Assert.assertEquals(20, result)
+    }
+
+    @Test
+    fun testCollectionApi() {
+        val list = listOf(1, "2", 3, 4, 5.7, 1, 2)
+        println(list.filter { item -> item is Int })
+        println(list.filter {it is Int}) // 키워드가 1개인 경우, it 키워드로 접근 가능
+        println(list.map { "value: ${it}" }) // 새로운 Collection 생성
+        println(list.filter {it is Int}.map { "value2: ${it}" })
+        println(list.find {it is Double})
+        val map = list.groupBy { it.javaClass } // 그룹화 -> Map<String, List<T>>
+        println(map)
+        val list2 = listOf(listOf(1,2), listOf("1",99.0)) // 중첩 Collection
+        println(list2)
+        println(list2.map { "value3: ${it}" })
+        println(list2.flatMap {it.toList()}) // 리스트 평평하게 하여 배열 반환
+    }
 }
